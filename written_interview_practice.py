e0e077318b8b15f8a08aa90ddf35488c61712687
uyennguyen16900 <uyennguyen16900@gmail.com>
2020-04-04 16:22:47 -0700

    first commit

---
+++ written_interview_practice.py
@@ -0,0 +1,3 @@
+# Find 2 new problems on LeetCode and follow the communication steps covered in last class to guide your thinking and problem solving.
+# Write down and commit your communication steps (in code comments) and solution code to a GitHub repository.
+# Submit your GitHub repository to Gradescope.
